cb27fad25016944682a1fbadc3b5cd5bc592ad1a
macuco <macuco8@hotmail.com>
2020-09-19 11:56:49 -0500

    directivas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'webminar-angular';
+  title:string = 'webminar-angular';
+  precio1:number = 50.50
+  precio2:number = 30.25
+  productoActivo:boolean = true;
+  productos:string[] = ["producto 1", "producto2", "producto3"]
+
+  sumarProductos(){
+    return this.precio1+this.precio2;
+  }
+
+  eventoBoton(){
+    this.productoActivo = !this.productoActivo;
+  }
 }
+
+
+class Producto {
+  title:string = '';
+  precio1:number = 0
+  precio2:number = 0
+  productoActivo:boolean = false;
+  productos:string[] = []
+}
+
